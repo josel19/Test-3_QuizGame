0e80239c8a85ff942afb310e5b358d3305cbd324
josel19 <jjlugogomez123@gmail.com>
2024-09-29 23:19:13 -0500

    Primera forma

--- Program.cs
+++ Program.cs
@@ -4,8 +4,99 @@ using Test_3_QuizGame.Models;
 Console.WriteLine("-- Quiz Game --");
 
 var questions = new List<Question>();
+var answers = new List<Answer>();
+string playerName;
+string? wanToPlay;
 
-SeedQuestionAndOptions();
+
+StartGame();
+
+void StartGame()
+{
+    GetPlayerName();
+    WantToPlay();
+}
+
+void GetPlayerName()
+{
+    try
+    {
+        Console.WriteLine("Hey!, What's your name?");
+        playerName = Console.ReadLine();
+
+        if (playerName == null)
+        {
+            throw new InvalidOperationException("Nombre no valido");
+        }
+    }
+    catch (Exception e) {
+        Console.WriteLine("Ha ocurrido un error: -"+e.Message);
+        GetPlayerName();
+    }
+}
+
+void WantToPlay()
+{
+    Console.WriteLine($"Nice!,{playerName} do you want to play a game? Yes/No");
+    string Answer = Console.ReadLine();
+
+    switch (Answer?.ToLower())
+    {
+        case "yes":
+            Play();
+            break;
+        case "no":
+            NoPlay();
+            break;
+        default:
+            Console.WriteLine("Please write Yes or No");
+            WantToPlay();
+            break;
+    }
+
+}
+void Play()
+{
+    Console.WriteLine("Let's play!");
+    SeedQuestionAndOptions();
+    PrintQuestions();
+}
+
+void NoPlay()
+{
+    Console.WriteLine("Ok, see you!");
+}
+
+void PrintQuestions()
+{
+    foreach (var question in questions)
+    {
+        Console.WriteLine($"{question.Id}- {question.QuestionText}.");
+        foreach (var option in question.Options)
+        {
+            Console.WriteLine($"    {option.id}. {option.Text}.");
+        }
+        GetAnswers(question.Id, question.Options);
+    }
+}
+
+void GetAnswers(int questionId, List<Option> options)
+{
+    string[] validOptions = { "1", "2", "3", "4" };
+    Console.WriteLine("Please, enter 1, 2 ,3 or 4");
+    var answer = Console.ReadLine();
+    if (!validOptions.Contains(answer)) 
+    {
+        GetAnswers(questionId, options);
+        return;
+    }
+    answers.Add(new Answer
+    {
+        QuestionId = questionId,
+        SelectedOption = options.Find(opt => opt.id == int.Parse(answer)),
+    });
+
+}
 
 void SeedQuestionAndOptions(){
 
